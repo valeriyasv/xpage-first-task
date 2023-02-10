8d67db6095a495a8bcb31101906ebf8667979673
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-02-11 01:42:13 +0300

    Исправление ошибок: хлебные крошки.Создание календаря

---
+++ src/js/burger.js
@@ -0,0 +1,18 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-header__burger');
+let imageBurger = document.querySelector('.main-header__burger-img');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  console.log('lsjfj')
+  imageBurger.classList.toggle('active');
+  console.log(navToggle.classList);
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,11 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-header__burger');
-let imageBurger = document.querySelector('.main-header__burger-img');
-navMain.classList.remove('main-nav--nojs');
+flatpickr('.calendar', {
+  "mode": "range",
+})
 
-navToggle.addEventListener('click', function () {
-  console.log('lsjfj')
-  imageBurger.classList.toggle('active');
-  console.log(navToggle.classList);
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-
-const selectSingle = document.querySelector('.select');
-const selectSingle_title = selectSingle.querySelector('.select__title');
-const selectSingle_labels = selectSingle.querySelectorAll('.select__label');
-
-// Toggle menu
-selectSingle_title.addEventListener('click', () => {
-  if ('active' === selectSingle.getAttribute('data-state')) {
-    selectSingle.setAttribute('data-state', '');
-  } else {
-    selectSingle.setAttribute('data-state', 'active');
-  }
-});
-
-// Close when click to option
-for (let i = 0; i < selectSingle_labels.length; i++) {
-  selectSingle_labels[i].addEventListener('click', (evt) => {
-    selectSingle_title.textContent = evt.target.textContent;
-    selectSingle.setAttribute('data-state', '');
-  });
+config = {
+  enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    "locale": Russian,
 }
 
-// Reset title
-const reset = document.querySelector('.reset');
-reset.addEventListener('click', () => {
-  selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
-});
+flatpickr('.calendar', config);
